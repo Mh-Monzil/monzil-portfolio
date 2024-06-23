f6656a10c73d819f104ea447bf9699ed6132ddf2
Mh-Monzil <monzil246@gmail.com>
2024-06-23 21:59:45 +0600

    animation fixed

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -8,17 +8,17 @@ const PageTransition = () => {
         initial={{x: "100%", width: "100%"}}
         animate={{x: "0%", width: "0%"}}
         exit={{x: ["0%", "100%"], width: ["0%", "100%"]}}
-        transition={{duration: 0.4, ease: "easeInOut"}}
+        transition={{duration: 0.6, ease: "easeIn"}}
         ></motion.div>
         <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-40 bg-white'
         initial={{x: "100%", width: "100%"}}
         animate={{x: "0%", width: "0%"}}
-        transition={{delay:0.2, duration: 0.4, ease: "easeInOut"}}
+        transition={{delay:0.2, duration: 0.6, ease: "easeIn"}}
         ></motion.div>
         <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary'
         initial={{x: "100%", width: "100%"}}
         animate={{x: "0%", width: "0%"}}
-        transition={{delay:0.4, duration: 0.4, ease: "easeInOut"}}
+        transition={{delay:0.4, duration: 0.6, ease: "easeIn"}}
         ></motion.div>
         </>
     );

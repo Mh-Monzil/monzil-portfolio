43113d6ead3a161af6d832f8ea596d7626729f2d
Mh-Monzil <monzil246@gmail.com>
2024-07-03 14:13:54 +0600

    add: footer added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -15,13 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${montserrat.className} bg-dark text-white min-h-screen max-w-7xl mx-auto`}
-      >
-        <Navbar />
-        <div className="p-4 md:p-6">
-        {children}
+      <body className={`${montserrat.className} bg-dark text-white `}>
+        <div className="min-h-screen max-w-7xl mx-auto">
+          <Navbar />
+          <div className="p-4 md:p-6">{children}</div>
         </div>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='flex justify-between items-center p-6 border-t font-medium'>
+            <p>© 2024 All Right Reserved </p>
+            <p>Built by <i>mohiuddin_monzil</i></p>
+        </div>
+    );
+};
+
+export default Footer;

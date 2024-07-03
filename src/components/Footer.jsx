5c7378a570b8f5d030a96c3e108d933d69969c8b
Mh-Monzil <monzil246@gmail.com>
2024-07-03 14:19:42 +0600

    update: footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='flex justify-between items-center p-6 border-t font-medium'>
+        <div className='flex justify-between items-center p-4 md:p-6 mt-10 md:mt-4 border-t font-medium'>
             <p>© 2024 All Right Reserved </p>
             <p>Built by <i>mohiuddin_monzil</i></p>
         </div>
